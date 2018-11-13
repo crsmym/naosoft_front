9c4ceb62d1660449d930f879c3bb2b4560e0ebe1
yeongminkang <yeongmin.kang@cfind.co.kr>
2018-11-13 21:13:22 +0900

    네이버 지도 api 연동 작업중

--- js/script.js
+++ js/script.js
@@ -1,7 +1,42 @@
 $(function(){
   $('.service_slide').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    centerMode: true,
+    slidesToShow: 1,
+    centerPadding: '20%',
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  })
+  $('.history_slide').slick({
+    centerMode: true,
+    infinite: true,
+    centerPadding: 0,
+    slidesToShow: 1,
+    vertical: true,
+    adaptiveHeight: true
+  })
+  $('.client_slide').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
   })
 })
