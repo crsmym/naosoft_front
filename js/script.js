0b3a6dec6aae7838257727b402cd255abff1a727
yeongminkang <yeongmin.kang@cfind.co.kr>
2018-11-15 21:37:07 +0900

    반응형 최적화 작업 진행중- 모바일 인터렉션 작업

--- js/script.js
+++ js/script.js
@@ -41,4 +41,52 @@ $(function(){
       }
     }, ]
   })
+
+  // mobile navigation menu slide toggle 
+  
+  $(".menu_btn a .open").click(function () {
+    event.preventDefault();
+    $(this).hide();
+    $(".menu_btn a .close").show();
+    $('#m_menu_list').animate({
+      right: 0
+    }, 500)
+    $('#dim').animate({
+      right: 0
+    }, 200)
+  });
+  
+  $(".menu_btn a .close").click(function () {
+    event.preventDefault();
+    $(this).hide();
+    $(".menu_btn a .open").show();
+    $('#m_menu_list').animate({
+      right: '-290px'
+    }, 500)
+    $('#dim').animate({
+      right: '-100%'
+    }, 200)
+  });
+  
+  // menu bar navigaiter 
+  $('.menu_li a').click(function(){
+    $('.menu_li a').removeClass('active');
+    $(this).addClass('active');
+  })
+  
+  // TOP button
+  $(window).scroll(function () {
+    scrollTop = $(window).scrollTop();
+    if (scrollTop > $(window).height()) {
+      $('.btn_top').addClass('on');
+    } else {
+      $('.btn_top').removeClass('on');
+    }
+  });
+  
+  // when the on click Top button
+  $('.btn_top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
 })
